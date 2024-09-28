242db7ba02f528c587e04342c482dccc68b51175
FeierYuu <1965214761@qq.com>
2024-09-29 04:44:31 +0800

    实现了 一次性制作多个模板文件的功能

--- FeierYuu-Generator-maker/src/main/java/com/feieryuu/maker/cli/command/ConfigCommand.java
+++ FeierYuu-Generator-maker/src/main/java/com/feieryuu/maker/cli/command/ConfigCommand.java
@@ -1,33 +1,33 @@
-package com.feieryuu.maker.cli.command;
-
-import cn.hutool.core.util.ReflectUtil;
-import com.feieryuu.maker.model.DataModel;
-import picocli.CommandLine;
-
-import java.lang.reflect.Field;
-
-/**
- * ClassName: ConfigCommand
- * Description:
- * date: 2024/9/11 3:56
- *
- * @author 飞飞鱼
- * @since JDK 1.8
- */
-@CommandLine.Command(name = "config",description = "查看参数信息",mixinStandardHelpOptions = true)
-
-public class ConfigCommand implements Runnable{
-
-    /**
-     * 通过反射获取信息
-     */
-    @Override
-    public void run() {
-        Field[] fields = ReflectUtil.getFields(DataModel.class);
-        for (Field field : fields) {
-            System.out.println("字段名称"+field.getName());
-            System.out.println("字段类型"+field.getType());
-            System.out.println("---------");
-        }
-    }
-}
+//package com.feieryuu.maker.cli.command;
+//
+//import cn.hutool.core.util.ReflectUtil;
+//import com.feieryuu.maker.model.DataModel;
+//import picocli.CommandLine;
+//
+//import java.lang.reflect.Field;
+//
+///**
+// * ClassName: ConfigCommand
+// * Description:
+// * date: 2024/9/11 3:56
+// *
+// * @author 飞飞鱼
+// * @since JDK 1.8
+// */
+//@CommandLine.Command(name = "config",description = "查看参数信息",mixinStandardHelpOptions = true)
+//
+//public class ConfigCommand implements Runnable{
+//
+//    /**
+//     * 通过反射获取信息
+//     */
+//    @Override
+//    public void run() {
+//        Field[] fields = ReflectUtil.getFields(DataModel.class);
+//        for (Field field : fields) {
+//            System.out.println("字段名称"+field.getName());
+//            System.out.println("字段类型"+field.getType());
+//            System.out.println("---------");
+//        }
+//    }
+//}
