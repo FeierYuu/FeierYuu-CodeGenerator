b465599e69e73b05dfdfb6dcbd4d474de4b3444e
FeierYuu <1965214761@qq.com>
2024-10-21 02:17:22 +0800

    新增文件配置页面

---
+++ FeierYuu-Generator-web-backend/src/main/java/com/feieryuu/web/model/dto/generator/GeneratorMakeRequest.java
@@ -0,0 +1,9 @@
+package com.feieryuu.web.model.dto.generator;/**
+* ClassName: GeneratorMakeRequest 
+* Description: 
+* date: 2024/10/20 21:43
+* @author 飞飞鱼
+* @version
+* @since JDK 1.8
+*/public class GeneratorMakeRequest {
+}
